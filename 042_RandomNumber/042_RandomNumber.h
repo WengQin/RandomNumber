037827a5c0e6a8ede31708c1da0cc189e96d0e84
abby <614171629@qq.com>
2019-01-06 10:36:25 +0800

    First commit

---
+++ 042_RandomNumber/042_RandomNumber.h
@@ -0,0 +1,32 @@
+
+// 042_RandomNumber.h : PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含“stdafx.h”以生成 PCH 文件"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CMy042_RandomNumberApp: 
+// 有关此类的实现，请参阅 042_RandomNumber.cpp
+//
+
+class CMy042_RandomNumberApp : public CWinApp
+{
+public:
+	CMy042_RandomNumberApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CMy042_RandomNumberApp theApp;
